760facedf10bab99e8582494c32ff38d43f01dd0
Andrea Falzetti <afalzetti@gmail.com>
2022-04-11 13:19:12 +0000

    create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.2/zsh-in-docker.sh)" -- \
+   -t https://github.com/denysdovhan/spaceship-prompt \
+   -a 'SPACESHIP_PROMPT_ADD_NEWLINE="false"' \
+   -a 'SPACESHIP_PROMPT_SEPARATE_LINE="false"' \
+   -p git \
+   -p ssh-agent \
+   -p https://github.com/zsh-users/zsh-autosuggestions \
+   -p https://github.com/zsh-users/zsh-completions
